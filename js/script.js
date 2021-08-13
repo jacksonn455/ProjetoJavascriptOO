80bd98c700f3f0990ede67c9dc68f7f2bad3867b
Jackson Felipe Magnabosco <jackson.magnabosco@pampa.compasso>
2021-08-12 21:56:13 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+class Produto { 
+
+    // todos atributos são colocados no metodo contrutor
+    constructor(){ 
+    // primeira função que é chamada na classe produto é o construtor
+
+    }
+
+    adicionar(){
+        alert("metodo adicionar");
+    }
+
+    excluir(){
+        alert("metodo excluir");
+    }
+
+}
+
+//instanciar objeto
+//normalmente a instancia tem o mesmo nome da classe so que minusculo
+
+var produto = new Produto();
